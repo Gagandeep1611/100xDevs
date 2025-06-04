d63808f9d9d7f672623e6c6b9710c2e8455130c1
Gagandeep Singh <gagandeep161198@gmail.com>
2025-06-04 19:24:36 +0530

    JWT token and authentication

--- Practice-workspace/Week3.js
+++ Practice-workspace/Week3.js
@@ -4,3 +4,13 @@ Two prechecks that any website does is:
 1. Authentication 
 2. Input validation 
 */
+
+/* Fetch: fetch represents how we can send http requests from real websites 
+Hashing: the process of encrypting passwords, it is one way.
+Encryption: encrpytion is two way and can be decrypted (unlike hashing)
+Json Web tokens: it takes JSON as input, takes it and creates a token, whoever has that token 
+can see the input. 
+Decoding a jw token is easy but verification can only be done by the backend. 
+
+Local Storage: 
+*/
