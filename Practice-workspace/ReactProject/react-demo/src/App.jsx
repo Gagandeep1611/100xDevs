52c9ce4a4545ed5b7a721baeadf09cbb51bd596f
Gagandeep Singh <gagandeep161198@gmail.com>
2025-06-10 00:02:10 +0530

    Update App.jsx

--- Practice-workspace/ReactProject/react-demo/src/App.jsx
+++ Practice-workspace/ReactProject/react-demo/src/App.jsx
@@ -1,16 +1,25 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
+  const [count, setCount] = useState(0);
   return (
     <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <CustomButton count = {count} setCount = {setCount}></CustomButton>
     </div>
   )
 }
 
+function CustomButton(props){
+  function onClickHandler(){
+    props.setCount(props.count + 1);
+  }
+return (
+  <button onClick ={onClickHandler}>
+    Counter {props.count}
+  </button>
+);
+
+}
 export default App
